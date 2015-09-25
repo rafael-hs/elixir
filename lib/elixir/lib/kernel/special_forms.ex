119c2fc4c02f846363072044ece2961181a766d4
eksperimental <eksperimental@users.noreply.github.com>
2015-09-26 04:15:23 +0700

    Improve Summaries
    It splits the long summaries
    and other fixes

--- lib/elixir/lib/kernel/special_forms.ex
+++ lib/elixir/lib/kernel/special_forms.ex
@@ -1,12 +1,11 @@
 defmodule Kernel.SpecialForms do
   @moduledoc """
-  In this module we define Elixir special forms. Special forms
-  cannot be overridden by the developer and are the basic
+  Special forms cannot be overridden by the developer and are the basic
   building blocks of Elixir code.
 
-  Some of those forms are lexical (like `alias`, `case`, etc).
-  The macros `{}` and `<<>>` are also special forms used to define
-  tuple and binary data structures respectively.
+  In this module we define them. Some of these forms are lexical (like
+  `alias`, `case`, etc). The macros `{}` and `<<>>` are also special
+  forms used to define tuple and binary data structures respectively.
 
   This module also documents Elixir's pseudo variables (`__ENV__`,
   `__MODULE__`, `__DIR__` and `__CALLER__`). Pseudo variables return
