296cb2bba2bad883f06f027744e6ed7cb95550c2
eksperimental <eksperimental@users.noreply.github.com>
2016-02-13 13:02:57 +0700

    Add Range functions: congruent?/2, disjoint?2, contiguous?/2, intersection/2, member?/2, overlaps?/2, reverse/1, size/1, sort/2, subset?/2, to_list/2, union/2

--- lib/elixir/lib/range.ex
+++ lib/elixir/lib/range.ex
@@ -75,6 +75,301 @@ defmodule Range do
   def range?(term)
   def range?(%Range{}), do: true
   def range?(_), do: false
+
+  @doc """
+  Determines if two ranges are congruent (i.e. that both cover the same range, regardless the order
+  of their limits).
+
+  ## Examples:
+
+      iex> Range.congruent?(-1..5, 5..-1)
+      true
+
+      iex> Range.congruent?(-1..5, -1..5)
+      true
+
+      iex> Range.congruent?(1..5, 5..-1)
+      false
+  """
+  @spec congruent?(t, t) :: boolean
+  def congruent?(range1, range2)
+  def congruent?(a..b, x..y) when (a == x and b == y) or (a == y and b == x),
+    do: true
+  def congruent?(_.._, _.._),
+    do: false
+
+  @doc """
+  Determines if two ranges are contiguous.
+
+  Note that the order of the limits does not matter to determine whether the ranges are contiguous
+  or not (eg. `1..3` and `4..6` are contiguous, same as `3..1` and `4..6`).
+
+  If the ranges overlap each other, it will return `false`.
+
+  ## Examples
+
+      iex> Range.contiguous?(1..4, 5..8)
+      true
+
+      iex> Range.contiguous?(1..4, 0..-4)
+      true
+
+      # Ranges overlap
+      iex> Range.contiguous?(1..4, 0..1)
+      false
+
+      iex> Range.contiguous?(1..4, 6..8)
+      false
+
+      iex> Range.contiguous?(1..5, 4..8)
+      false
+  """
+  @spec contiguous?(t, t) :: boolean
+  def contiguous?(_.._ = range1, _.._ = range2) do
+    a..b = sort(range1)
+    x..y = sort(range2)
+    if (b + 1 == x) or (y + 1 == a) do
+      true
+    else
+      false
+    end
+  end
+
+  @doc """
+  Checks if `range1` and `range2` have no members in common.
+
+  ## Examples
+
+      iex> Range.disjoint?(1..2, 3..4)
+      true
+      iex> Range.disjoint?(1..2, 2..3)
+      false
+
+  """
+  @spec disjoint?(t, t) :: boolean
+  def disjoint?(a..b = range1, x.._ = range2) do
+    not( (a in range2) or (b in range2) or (x in range1) )
+  end
+
+  @doc """
+  Returns a range containing the range shared in common between `range1` and
+  `range2`.
+  Returns `nil` if there is no intersection.
+
+  Note that the returned range, if any, will always be in ascending order (the
+  first element is <= the last element).
+
+  ## Examples
+
+      iex> Range.intersection(1..5, 4..8)
+      4..5
+
+      iex> Range.intersection(-1..4, -3..8)
+      -1..4
+
+      iex> Range.intersection(1..4, 4..8)
+      4..4
+
+      iex> Range.intersection(5..5, 5..5)
+      5..5
+
+      iex> Range.intersection(1..5, 6..8)
+      nil
+  """
+  @spec intersection(t, t) :: t | nil
+  def intersection(range1, range2)
+  def intersection(a..b = range1, x..y = _range2) when (a == x and b == y) or (a == y and b == x) do
+    sort(range1)
+  end
+
+  def intersection(_.._ = range1, _.._ = range2) do
+    if overlaps?(range1, range2) do
+      a..b = sort(range1)
+      x..y = sort(range2)
+      max(a, x)..min(b, y)
+    else
+      nil
+    end
+  end
+
+  @doc """
+  Checks if `ranges` contains `value`.
+
+  ## Examples
+
+      iex> Range.member?(1..3, 2)
+      true
+      iex> Range.member?(1..3, 4)
+      false
+      iex> Range.member?(1..3, :foo)
+      false
+      iex> Range.member?(1..3, 1..3)
+      false
+  """
+  @spec member?(t, term) :: boolean
+  def member?(first..last = range, value) when is_integer(value) do
+    if first <= last do
+      first <= value and value <= last
+    else
+      last <= value and value <= first
+    end
+  end
+
+  def member?(_.._, _value) do
+    false
+  end
+
+  @doc """
+  Determines if two ranges overlap each other.
+
+  ## Examples
+
+      iex> Range.overlaps?(1..5, 4..6)
+      true
+
+      iex> Range.overlaps?(1..5, 7..9)
+      false
+
+      iex> Range.overlaps?(5..1, 6..4)
+      true
+  """
+  @spec overlaps?(t, t) :: boolean
+  def overlaps?(a..b = range1, x.._ = range2) do
+    (a in range2) or (b in range2) or (x in range1)
+  end
+
+  @doc """
+  Swaps the order of the first and last limits in the range.
+
+  ## Examples
+
+    iex> Range.reverse(0..10)
+    10..0
+
+    iex> Range.reverse(10..0)
+    0..10
+  """
+  @spec reverse(t) :: t
+  def reverse(range)
+  def reverse(first..last) do
+    last..first
+  end
+
+  @doc """
+  Returns the number of elements in `range`.
+
+  ## Examples
+
+      iex> Range.size(-5..5)
+      11
+
+      iex> Range.size(3..3)
+      1
+  """
+  @spec size(t) :: pos_integer
+  def size(range)
+  def size(first..first),
+    do: 1
+  def size(first..last) when first < last,
+    do: last - first + 1
+  def size(first..last) when first > last,
+    do: first - last + 1
+
+  @doc """
+  Sorts the `range` by the given `order`.
+
+  `order` can take either `:ascending` or `:descending`.
+  If no `order` is specified, `:ascending` order is used.
+
+  ## Examples
+
+      iex> Range.sort(0..10)
+      0..10
+
+      iex> Range.sort(10..0)
+      0..10
+
+      iex> Range.sort(0..10, :descending)
+      10..0
+  """
+  @spec sort(t, :ascending | :descending) :: t
+  def sort(range, order \\ :ascending)
+
+  def sort(first..last, order)
+  when (order == :ascending and first > last) or (order == :descending and last > first) do
+    last..first
+  end
+
+  def sort(_first.._last = range, order) when order in [:ascending, :descending] do
+    range
+  end
+
+  @doc """
+  Checks if `range1`'s members are all contained in `range2`.
+
+  This function checks if `range1` is a subset of `range2`.
+
+  ## Examples
+
+      iex> Range.subset?(1..2, 1..3)
+      true
+      iex> Range.subset?(1..3, 1..2)
+      false
+
+  """
+  @spec subset?(t, t) :: boolean
+  def subset?(a..b = _range1, range2) do
+    (a in range2) and (b in range2)
+  end
+
+  @doc """
+  Converts `range` to a list.
+
+  ## Examples
+
+      iex> Range.to_list(1..3)
+      [1, 2, 3]
+
+  """
+  @spec to_list(t) :: list
+  def to_list(_.._ = range) do
+    Enum.to_list(range)
+  end
+
+  @doc """
+  Returns the union of two ranges, or `nil` if there is no union.
+
+  A union the represented by the smallest element and the biggest integers in
+  both ranges, provided that the ranges overlap or are contiguous.
+
+  Note that the returned range, if any, will be in ascending order.
+
+  ## Examples
+
+      iex> Range.union(1..3, 4..6)
+      1..6
+
+      iex> Range.union(1..5, 4..8)
+      1..8
+
+      iex> Range.union(3..6, 1..3)
+      1..6
+  """
+  @spec union(t, t) :: t | nil
+  def union(range1, range2)
+  def union(a..b = range1, x..y = _range2) when (a == x and b == y) or (a == y and b == x) do
+    sort(range1)
+  end
+
+  def union(_.._ = range1, _.._ = range2) do
+    if overlaps?(range1, range2) or contiguous?(range1, range2) do
+      a..b = sort(range1)
+      x..y = sort(range2)
+      min(a, x)..max(b, y)
+    else
+      nil
+    end
+  end
 end
 
 defimpl Enumerable, for: Range do
@@ -102,25 +397,11 @@ defimpl Enumerable, for: Range do
     {:done, acc}
   end
 
-  def member?(first .. last, value) when is_integer(value) do
-    if first <= last do
-      {:ok, first <= value and value <= last}
-    else
-      {:ok, last <= value and value <= first}
-    end
-  end
+  def member?(range, value),
+    do: {:ok, Range.member?(range, value)}
 
-  def member?(_ .. _, _value) do
-    {:ok, false}
-  end
-
-  def count(first .. last) do
-    if first <= last do
-      {:ok, last - first + 1}
-    else
-      {:ok, first - last + 1}
-    end
-  end
+  def count(_.._ = range),
+    do: {:ok, Range.size(range)}
 end
 
 defimpl Inspect, for: Range do
--- lib/elixir/test/elixir/range_test.exs
+++ lib/elixir/test/elixir/range_test.exs
@@ -17,6 +17,7 @@ defmodule RangeTest do
 
   test "range?" do
     assert Range.range?(1..3)
+    refute Range.range?([1, 2, 3])
     refute Range.range?(0)
   end
 
@@ -56,6 +57,233 @@ defmodule RangeTest do
       first..last
       Enum.map(first..last, &(&1))
     end
+  end
 
+  test "invalid arguments" do
+    list = [1, 2, 3, 4]
+    range = 5..8
+
+    # arity 1
+    for f <- [:reverse, :sort] do
+      assert_raise FunctionClauseError, fn ->
+        apply(Range, f, [list])
+      end
+    end
+
+    # arity 2
+    for f <- [:congruent?, :contiguous?, :intersection, :overlaps?, :union] do
+      assert_raise FunctionClauseError, fn ->
+        apply(Range, f, [list, range])
+      end
+      assert_raise FunctionClauseError, fn ->
+        apply(Range, f, [range, list])
+      end
+      assert_raise FunctionClauseError, fn ->
+        apply(Range, f, [list, list])
+      end
+    end
+  end
+
+  test "disjoint?/2" do
+    assert Range.disjoint?(1..4, 5..8)
+    refute Range.disjoint?(0..2, 1..4)
+
+    # Touching
+    refute Range.disjoint?(4..4, 4..4)
+    refute Range.disjoint?(0..1, 1..4)
+    refute Range.disjoint?(2..4, 4..2)
+
+    # Not touching
+    assert Range.disjoint?(1..4, 6..8)
+    assert Range.disjoint?(1..4, -1..-4)
+    assert Range.disjoint?(1..4, -4..-1)
+
+    # Contiguous
+    assert Range.disjoint?(1..4, 5..8)
+    assert Range.disjoint?(1..4, 0..-4)
+    assert Range.disjoint?(1..4, -4..0)
+  end
+
+  test "congruent?/2" do
+    assert Range.congruent?(1..4, 1..4)
+    assert Range.congruent?(1..4, 4..1)
+    assert Range.congruent?(4..4, 4..4)
+    assert Range.congruent?(4..4, 4..4)
+
+    refute Range.congruent?(1..4, 5..8)
+    refute Range.congruent?(1..4, -1..-4)
+    refute Range.congruent?(1..4, 1..5)
+    refute Range.congruent?(0..4, 1..5)
+    refute Range.congruent?(4..4, 3..3)
+  end
+
+  test "contiguous?/2" do
+    assert Range.contiguous?(1..4, 5..8)
+    assert Range.contiguous?(1..4, 0..-4)
+    assert Range.contiguous?(1..4, -4..0)
+
+    # Overlaping ranges
+    refute Range.contiguous?(4..4, 4..4)
+    refute Range.contiguous?(0..1, 1..4)
+    refute Range.contiguous?(0..2, 1..4)
+    refute Range.contiguous?(2..4, 4..2)
+
+    # Not touching
+    refute Range.contiguous?(1..4, 6..8)
+    refute Range.contiguous?(1..4, -1..-4)
+    refute Range.contiguous?(1..4, -4..-1)
+  end
+
+  test "intersection/2" do
+    assert Range.intersection(1..5, 4..8) == 4..5
+    assert Range.intersection(5..1, 4..8) == 4..5
+    assert Range.intersection(1..5, 8..4) == 4..5
+    assert Range.intersection(5..1, 8..4) == 4..5
+
+    assert Range.intersection(1..4, 4..8) == 4..4
+    assert Range.intersection(4..1, 4..8) == 4..4
+    assert Range.intersection(1..4, 8..4) == 4..4
+    assert Range.intersection(4..1, 8..4) == 4..4
+
+    assert Range.intersection(-1..4, -3..8) == -1..4
+    assert Range.intersection(-1..4, -3..3) == -1..3
+
+    # same ranges
+    assert Range.intersection(4..4, 4..4) == 4..4
+    assert Range.intersection(4..-1, 4..-1) == -1..4
+    assert Range.intersection(-1..4, -1..4) == -1..4
+    assert Range.intersection(4..-1, -1..4) == -1..4
+    assert Range.intersection(-1..4, 4..-1) == -1..4
+
+    # contiguous ranges
+    assert Range.intersection(0..1, 2..3) == nil
+    assert Range.intersection(0..1, 3..2) == nil
+    assert Range.intersection(1..1, 2..2) == nil
+
+    # Not touching
+    assert Range.intersection(1..4, 6..8) == nil
+    assert Range.intersection(1..4, -1..-4) == nil
+    assert Range.intersection(1..4, -4..-1) == nil
+  end
+
+  test "member?/1" do
+    assert Range.member?(1..3, 1)
+    assert Range.member?(1..3, 2)
+    assert Range.member?(1..3, 3)
+    refute Range.member?(1..3, 0)
+    refute Range.member?(1..3, 4)
+    refute Range.member?(3..1, 0)
+    refute Range.member?(3..1, 4)
+    
+    assert Range.member?(3..3, 3)
+    refute Range.member?(3..3, 4)
+
+    refute Range.member?(1..3, :foo)
+    refute Range.member?(1..3, 1..3)
+    refute Range.member?(1..3, 2..2)
+  end
+
+  test "overlaps?/2" do
+    assert Range.overlaps?(4..4, 4..4)
+    assert Range.overlaps?(0..1, 1..4)
+    assert Range.overlaps?(0..2, 1..4)
+    assert Range.overlaps?(2..4, 4..2)
+
+    # Contiguous
+    refute Range.overlaps?(1..4, 5..8)
+    refute Range.overlaps?(1..4, 0..-4)
+    refute Range.overlaps?(1..4, -4..0)
+
+    # Not touching
+    refute Range.overlaps?(1..4, 6..8)
+    refute Range.overlaps?(1..4, -1..-4)
+    refute Range.overlaps?(1..4, -4..-1)
+  end
+
+  test "reverse/1" do
+    assert Range.reverse(1..5) == 5..1
+    assert Range.reverse(5..5) == 5..5
+    assert Range.reverse(-5..1) == 1..-5
+  end
+
+  test "size/1" do
+    assert Range.size(1..5) == 5
+    assert Range.size(5..0) == 6
+    assert Range.size(-5..5) == 11
+    assert Range.size(-5..-5) == 1
+  end
+
+  test "sort/2" do
+    assert Range.sort(1..5) == 1..5
+    assert Range.sort(5..1) == 1..5
+    assert Range.sort(5..5) == 5..5
+    assert Range.sort(-1..-5) == -5..-1
+    assert Range.sort(-5..-1) == -5..-1
+    assert Range.sort(-1..-5, :ascending) == -5..-1
+    assert Range.sort(-5..-1, :ascending) == -5..-1
+
+    assert Range.sort(1..5, :descending) == 5..1
+    assert Range.sort(5..1, :descending) == 5..1
+    assert Range.sort(5..5, :descending) == 5..5
+    assert Range.sort(-1..-5, :descending) == -1..-5
+    assert Range.sort(-5..-1, :descending) == -1..-5
+
+    assert_raise FunctionClauseError, fn ->
+      Range.sort([1, 2, 3, 4], :descending)
+    end
+
+    assert_raise FunctionClauseError, fn ->
+      Range.sort(1..5, "descending")
+    end
+
+    assert_raise FunctionClauseError, fn ->
+      Range.sort(1..5, :asc)
+    end
+  end
+
+  test "subset?/2" do
+    assert Range.subset?(1..5, 0..6)
+    refute Range.subset?(0..6, 1..5)
+    assert Range.subset?(5..5, 5..5)
+    assert Range.subset?(5..5, 4..5)
+    refute Range.subset?(5..5, 4..4)
+  end
+
+  test "union/2" do
+    assert Range.union(1..5, 4..8) == 1..8
+    assert Range.union(5..1, 4..8) == 1..8
+    assert Range.union(1..5, 8..4) == 1..8
+    assert Range.union(5..1, 8..4) == 1..8
+
+    assert Range.union(1..4, 4..8) == 1..8
+    assert Range.union(4..1, 4..8) == 1..8
+    assert Range.union(1..4, 8..4) == 1..8
+    assert Range.union(4..1, 8..4) == 1..8
+
+    assert Range.union(-1..4, -3..8) == -3..8
+    assert Range.union(-1..4, -3..3) == -3..4
+
+    # same ranges
+    assert Range.union(4..4, 4..4) == 4..4
+    assert Range.union(4..-1, 4..-1) == -1..4
+    assert Range.union(-1..4, -1..4) == -1..4
+    assert Range.union(4..-1, -1..4) == -1..4
+    assert Range.union(-1..4, 4..-1) == -1..4
+
+    # contiguous ranges
+    assert Range.union(2..3, 0..1) == 0..3
+    assert Range.union(3..2, 0..1) == 0..3
+    assert Range.union(1..1, 2..2) == 1..2
+
+    # Not touching
+    assert Range.union(1..4, 6..8) == nil
+    assert Range.union(1..4, -1..-4) == nil
+    assert Range.union(1..4, -4..-1) == nil
+  end
+
+  test "to_list/1" do
+    assert Range.to_list(-1..3) == [-1, 0, 1, 2, 3]
+    assert Range.to_list(3..-1) == [3, 2, 1, 0, -1]
+    assert Range.to_list(3..3) == [3]
   end
 end
